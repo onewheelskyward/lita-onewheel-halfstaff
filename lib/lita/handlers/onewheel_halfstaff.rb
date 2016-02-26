29b2e98ce360f8d9e15d5e2272a4209890af1a0a
Andrew Kreps <andrew.kreps@gmail.com>
2016-02-25 17:14:17 -0800

    Put the pieces of date parsing into place.  It's goofball.

--- lib/lita/handlers/onewheel_halfstaff.rb
+++ lib/lita/handlers/onewheel_halfstaff.rb
@@ -1,9 +1,54 @@
+require 'nokogiri'
+
 module Lita
   module Handlers
     class OnewheelHalfstaff < Handler
       route /^halfstaff$/,
             :get_flag_status,
             command: true
+
+      def get_flag_status(response)
+        get_flag_data
+      end
+
+      def get_flag_data
+        flag_html = RestClient.get 'http://www.flagsexpress.com/HalfStaff_s/1852.htm'
+        noko_flag = Nokogiri::HTML flag_html
+        noko_flag.css('a').each do |a_tag|
+          if a_tag['href'].match /http\:\/\/www\.flagsexpress\.com\/Articles\.asp\?ID\=/i
+            goober = parse_flag_status_text(a_tag.text)
+            # puts goober[:date]
+            # puts goober[:place]
+            # puts goober[:desc]
+          end
+        end
+      end
+
+      def parse_flag_status_text(text)
+        pieces = text.split(/ - /)
+        if pieces[0].match(/ 2016/)
+          # puts pieces.inspect
+          if date_matches = pieces[0].match(/(\w+\s+\d+,\s+\d+)/)
+            puts 'Standard'
+            puts date_matches[1]
+          elsif date_matches = pieces[0].match(/(\w+)\s+(\d+)-(\d+)/)
+            puts 'Date range'
+            puts date_matches[1]
+            puts date_matches[2]
+            puts date_matches[3]
+          elsif date_matches = pieces[0].match(/(\w+\s+\d+) until sunset \w+, (\w+\s+\d+)/)
+            puts 'until sunset'
+            puts date_matches[1]
+            puts date_matches[2]
+          else
+            puts "Couldn't match #{pieces[0]}"
+          end
+
+          # puts date_matches.inspect
+        end
+
+        # return matches[1], matches[2], matches[3]
+      end
 
       Lita.register_handler(self)
     end
--- spec/lita/handlers/onewheel_halfstaff_spec.rb
+++ spec/lita/handlers/onewheel_halfstaff_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
+require 'rest-client'
 
 describe Lita::Handlers::OnewheelHalfstaff, lita_handler: true do
   it { is_expected.to route_command('halfstaff') }
 
+  it 'gives half-staff status' do
+    send_command 'halfstaff'
+  end
 end
